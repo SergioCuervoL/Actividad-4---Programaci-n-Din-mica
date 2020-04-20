cd5b9c32a68dad6935ee9d9612e1d1a9d752f78a
Sergio Andres Cuervo Leon <scuervoleon@uniminuto.edu.co>
2020-04-20 17:13:34 -0500

    Se incorpora programa inicial

---
+++ Actividad 4 - Programación Dinamica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Actividad_4_Programación_Dinamica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
