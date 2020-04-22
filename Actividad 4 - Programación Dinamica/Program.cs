308192a51df79c7758a66c791e14e3cbcd8046a9
jessi leaño <nata990615@hotmail.com>
2020-04-21 19:50:45 -0500

    Cambio incorporado, para que me lea el numero para saber el tamaño de la varilla

--- Actividad 4 - Programación Dinamica/Program.cs
+++ Actividad 4 - Programación Dinamica/Program.cs
@@ -4,9 +4,28 @@ namespace Actividad_4_Programación_Dinamica
 {
     class Program
     {
+        int[,] varilla = {{1,2,3,4,5,6,7,8,9,10},
+                          {1,5,8,9,10,17,17,20,24,30 }};
+
+        public int n = 0;
+
+        public int getVarilla()
+        {
+            Console.WriteLine("Ingrese el tamaño de la Varilla: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            return num;
+        }
+
+        public void ejecutar()
+        {
+            this.n = getVarilla();
+            Console.WriteLine(this.n);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program vari = new Program();
+            vari.ejecutar();
         }
     }
 }
