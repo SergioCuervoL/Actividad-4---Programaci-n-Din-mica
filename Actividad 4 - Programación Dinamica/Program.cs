3506a0793aae4452905b323f653ab9c9d6fe93f2
jessi leaño <nata990615@hotmail.com>
2020-04-24 18:25:18 -0500

    Cambio de nombres

--- Actividad 4 - Programación Dinamica/Program.cs
+++ Actividad 4 - Programación Dinamica/Program.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 
-namespace Actividad_4_Programación_Dinamica
+namespace Actividad_Programación_Dinamica
 {
     class Program
     {
-        int[,] varilla = {{1,2,3,4,5,6,7,8,9,10},
-                          {1,5,8,9,10,17,17,20,24,30 }};
+        int[,] tamvarilla = {{1,2,3,4,5,6,7,8,9,10},
+                            {1,5,8,9,10,17,17,20,24,30 }};
 
-        public int n = 0;
+        public int m = 0;
         public int res = 0;
         
-
         public int getVarilla()
         {
             int num = 0;
@@ -19,24 +18,18 @@ namespace Actividad_4_Programación_Dinamica
             return num;
         }
 
-        public void ejecutar()
-        {
-            this.n = getVarilla();
-            int[] lb = buscarBeneficios(this.n);
-            this.res = buscarMejorBeneficio(lb);
-            Console.WriteLine("El beneficio total es de: "+this.res);
-        }
-        public int [] buscarBeneficios (int tv)
+        public int [] solucionOptima (int tv)
         {
             int [] listaBeneficios = new int [10];
             for (int i = 0; i < 10; i++)
             {
-                int r = (int) Math.Floor(Convert.ToDouble (tv/ varilla[0, i]));
-                listaBeneficios[i] = r * varilla[1, i];
+                int r = (int) Math.Floor(Convert.ToDouble (tv/ tamvarilla[0, i]));
+                listaBeneficios[i] = r * tamvarilla[1, i];
             }
             return listaBeneficios;
         }
-        public int buscarMejorBeneficio(int[] listaBenificios)
+
+        public int buscarSolucionMasOptima(int[] listaBenificios)
         {
             int bmb = listaBenificios[0];
             int precio = 0;
@@ -47,18 +40,26 @@ namespace Actividad_4_Programación_Dinamica
                 {
                     bmb = listaBenificios[i];
                     longitud = i+1;
-                    precio = varilla[1, i];
+                    precio = tamvarilla[1, i];
                 }
             }
-            Console.WriteLine("La longitud que mas beneficia es de: "+longitud);
-            Console.WriteLine("El precio al que se deben vender es de: "+precio);
+            Console.WriteLine("La longitud en la que se va a dividir: "+longitud);
+            Console.WriteLine("El precio al que se vende es: "+precio);
             return bmb;
+        }
+
+        public void proceso()
+        {
+            this.m = getVarilla();
+            int[] lb = solucionOptima(this.m);
+            this.res = buscarSolucionMasOptima(lb);
+            Console.WriteLine("El valor total es de: " + this.res);
         }
 
         static void Main(string[] args)
         {
             Program vari = new Program();
-            vari.ejecutar();
+            vari.proceso();
         }
     }
 }
